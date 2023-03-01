fa1040fb38cdbcd766f7dae04dd41ce2c25dfb53
peshkovmaks <peshkovmaks@gmail.com>
2023-03-01 16:32:07 +0300

    add new game "brain_calc"

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-from random import randint
-import prompt
+from random import randint, choice
+import prompt, operator
 
 
-def even_game():
+def calc_game():
     print("Welcome to Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello,  {name.lower().title()}!")
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('What is the result of the expression?')
     i = 0
     while i < 3:
-        random_number = randint(1, 99)
-        if random_number % 2 == 0:
-            correct_answer = "yes"
-        else:
-            correct_answer = "no"
-        print(f"Question: {random_number}")
+        random_number_first = randint(1, 99)
+        random_number_second = randint(1, 99)
+        list_operation = {'*': operator.mul, '-': operator.sub, '+': operator.add}
+        random_operation = choice(list(list_operation))
+        operation = list_operation[random_operation]
+        expression = f'{random_number_first} {random_operation} {random_number_second}'
+        correct_answer = operation(random_number_first, random_number_second)
+        print(expression)
         user_answer = prompt.string("Your answer? ")
         if str(user_answer) == str(correct_answer):
             print("Correct!")
@@ -28,9 +30,10 @@ def even_game():
     print(f"Congratulations {name.lower().title()}!")
 
 
+
 def main():
-    even_game()
+    calc_game()
 
 
 if __name__ == "__main__":
-    main()
+     main()
