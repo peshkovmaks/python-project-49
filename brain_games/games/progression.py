170891c62cf8628090d3baf2baf452b6e72105f5
peshkovmaks <peshkovmaks@gmail.com>
2023-03-04 20:15:29 +0300

    fix linter error

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,8 +11,8 @@ def game_conditions():
     progression_start = randint(1, 15)
     progression_step = randint(6, 9)
     progression_end = (progression_step + 1) * (randint(6, 13))
-    progression = list(range(progression_start, progression_end, 
-                             progression_step))
+    progression = list(
+        range(progression_start, progression_end, progression_step))
     # specify hidden number and hidden symbol
     index_hidden_number = randint(0, len(progression) - 1)
     correct_answer = progression[index_hidden_number]
